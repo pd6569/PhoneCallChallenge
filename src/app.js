847013c2d8071e8a750ea607beadf1df6d48d255
Peter de Souza <peterdesouza@gmail.com>
2017-09-02 11:14:34 +0100

    basic function to analyse active call data added

--- src/app.js
+++ src/app.js
@@ -10,9 +10,20 @@ class PhoneCallApp {
         console.log("PhoneCallAppCreated");
         this.loadData();
 
-        // Level 1 data
-        this.level1GroupData = [];
-        this.level1AgentData = [];
+        // Level 1 group data
+        this.level1RawGroupData = [];
+        this.level1FilteredGroupData = {};
+
+        // Level 1 agent data
+        this.level1RawAgentData = [];
+        this.level1FilteredAgentData = {};
+
+        // Headers
+        this.groupDataHeaders = [];
+        this.agentDataHeaders = [];
+
+        // Get DOM elements
+        this.$level1 = $('#level1');
     }
 
     loadData(){
@@ -21,25 +32,83 @@ class PhoneCallApp {
         $.ajax({
             'url': 'data/20170418_000000_agent_statistics.csv',
             'success': function(data) {
-                _this.level1AgentData = _this._convertCSVtoJSON(data);
+                _this.level1RawAgentData = _this._convertCSVtoJSON(data, "agent");
+                _this.initLevel1Analysis("agent");
             }
         });
 
         $.ajax({
             'url': 'data/20170418_000000_group_statistics.csv',
             'success': function(data) {
-                _this.level1GroupData = _this._convertCSVtoJSON(data);
+                _this.level1RawGroupData = _this._convertCSVtoJSON(data, "group");
+                _this.initLevel1Analysis("group");
             }
         })
     }
+
+    initLevel1Analysis(dataType){
+        if (dataType === "group"){
+            this.generateActiveCallData(); // data contains empty data for out of hours (e.g. between midnight - 8:00, and after 18:30).
+            this.analyseActiveCallData();
+        }
+
+        if (dataType === "agent"){
+
+        }
+    }
+
+    generateActiveCallData(){
+        console.log("generateActiveCallData");
+        this.level1FilteredGroupData.activeCallData = [];
+        for (let row of this.level1RawGroupData){
+            if (row["avg. wait time "] !== "00:00:00") {
+                this.level1FilteredGroupData.activeCallData.push(row);
+            }
+        }
+
+        console.log("Active call data: ", this.level1FilteredGroupData.activeCallData)
+    }
+
+    analyseActiveCallData(){
+
+        let activeData = this.level1FilteredGroupData.activeCallData;
+
+        // Get first and last call times
+        let numActiveTimepoints = activeData.length;
+        this.level1FilteredGroupData.firstCallTimestamp = activeData[0]["timestamp"];
+        this.level1FilteredGroupData.lastCallTimestamp = activeData[numActiveTimepoints - 1]["timestamp"];
+        this.$level1.append(`<p><strong>First call data obtained at:</strong> ${this.level1FilteredGroupData.firstCallTimestamp}`);
+        this.$level1.append(`<p><strong>Last call data obtained at:</strong> ${this.level1FilteredGroupData.lastCallTimestamp}`);
+
+        // Get totals
+        for (let heading of this.groupDataHeaders){
+            if (heading !== "timestamp"){
+                let total = 0;
+                for (let row of activeData){
+                    let val = parseInt(row[heading]);
+                    total += val;
+                }
+                this.$level1.append(`<p><strong>Total ${heading}:</strong> ${total}`);
+            }
+        }
+    }
     
-    _convertCSVtoJSON(csv) {
+    _convertCSVtoJSON(csv, dataType) {
 
         let lines = csv.split("\n"); // creates an array of lines - first line is headings
 
         let result = []; // array to store each row of data as an object
 
         let headers = lines[0].split(","); // create array from headings
+
+        if (dataType === "group" && this.groupDataHeaders.length == 0) {
+            this.groupDataHeaders = headers;
+        }
+
+        if (dataType === "agent" && this.agentDataHeaders.length == 0) {
+            this.agentDataHeaders = headers;
+        }
+
 
         // loop through data row by row (starting at second line which is the data, not headings)
         for(let i = 1; i < lines.length; i++){
@@ -59,7 +128,6 @@ class PhoneCallApp {
                 // Store each row object in result array
                 result.push(obj);
             }
-
 
         }
 
